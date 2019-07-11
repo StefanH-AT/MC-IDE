96e3391f069aa407c09120e30dabe0d3ee76a15a
Tewan <tewan@spycesoftware.com>
2019-07-11 15:33:02 +0200

    Added Close Confirmation dialog

--- src/main/java/at/tewan/mcide/app/MainApplication.java
+++ src/main/java/at/tewan/mcide/app/MainApplication.java
@@ -4,19 +4,28 @@ import at.tewan.mcide.Resources;
 import at.tewan.mcide.project.Project;
 import at.tewan.mcide.util.Themes;
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ButtonBar;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Dialog;
+import javafx.scene.control.DialogPane;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+import javafx.stage.WindowEvent;
 
 import java.io.File;
+import java.util.Optional;
 
 
 public class MainApplication extends Application {
 
     private File projectConfigFile;
+
 
     public MainApplication(File path) throws Exception {
         projectConfigFile = path;
@@ -37,8 +46,27 @@ public class MainApplication extends Application {
         Scene mainScene = new Scene(root);
         primaryStage.setScene(mainScene);
 
+        primaryStage.setOnCloseRequest(closeEvent);
+
         primaryStage.show();
 
     }
+
+    private EventHandler<WindowEvent> closeEvent = event ->  {
+        Dialog closeConfirmationDialog = new Dialog();
+        DialogPane closeConfirmationDialogPane = closeConfirmationDialog.getDialogPane();
+
+        closeConfirmationDialog.setTitle("Close Confirmation");
+        closeConfirmationDialog.setContentText("Are you sure you want to exit?");
+
+        closeConfirmationDialogPane.getStylesheets().addAll(Themes.getCurrentTheme(), Themes.DEFAULT);
+        closeConfirmationDialogPane.getButtonTypes().addAll(ButtonType.YES, ButtonType.NO);
+
+        Optional result = closeConfirmationDialog.showAndWait();
+        if(result.isPresent() && result.get() == ButtonType.NO) {
+            event.consume();
+        }
+
+    };
 
 }
